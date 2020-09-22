6d17e6587ce093991242e949b92c7f8733e11e8a
dfrnks <douglasfrancardoso@gmail.com>
2020-09-22 19:27:24 -0300

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,87 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <a v-on:click="logout">Logout</a>
     </div>
-    <router-view/>
+    <router-view id="content"/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "Login",
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem("isLogged")
+      this.$router.go()
+    }
+  }
+}
+</script>
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  body, html
+    margin 0
+    background whitesmoke
+  #app
+    font-family Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #ff3a72
+    #nav
+      padding 20px
+      background #1d1d1d
+    #content
+      margin-top 20px
+  a
+    text-decoration none
+    cursor pointer
+    color #ff3a72
+  button
+    height: 38px;
+    padding: 0 30px;
+    text-align: center;
+    font-size: 11px;
+    line-height: 38px;
+    cursor: pointer;
+    box-sizing: border-box;
+
+    align-items: center;
+    border-radius: 4px;
+    display: inline-flex;
+    flex: 0 0 auto;
+    font-weight: 500;
+    letter-spacing: .0892857143em;
+    justify-content: center;
+    outline: 0;
+    position: relative;
+    text-decoration: none;
+    text-indent: .0892857143em;
+    text-transform: uppercase;
+    transition-duration: .28s;
+    transition-property: box-shadow,transform,opacity;
+    transition-timing-function: cubic-bezier(.4,0,.2,1);
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+
+    color: #FFF;
+    background-color: #33C3F0;
+    border-color: #33C3F0;
+
+  input
+    border-radius: 50px;
+    border-color: #e2e2e2;
+    padding: 8px;
+    border-width: 1px;
+    border-style: solid;
+  input:focus
+    border-color #ff3a72
+  *:focus
+    outline none
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div>
+      <div class="row">
+        <label>
+          Username:
+          <input v-model="username" type="text" placeholder="user">
+        </label>
+      </div>
+      <div class="row">
+        <label>
+          Password:
+          <input v-model="password" :type="password_type" placeholder="password">
+        </label>
+        <button class="btn-show-password" v-on:click="showPassword">Show</button>
+      </div>
+      <div v-if="mensagem_login">
+        <h5 :class="{'text-danger': has_error}">{{ mensagem_login }}</h5>
+      </div>
+      <div class="row">
+        <button v-on:click="login">login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data: () => ({
+    username: "fake",
+    password: "fake123",
+    password_type: "password",
+    has_error: false,
+    mensagem_login: null
+  }),
+  methods: {
+    showPassword() {
+      if (this.password_type === "password")
+        this.password_type = "text"
+      else
+        this.password_type = "password"
+    },
+    login: function () {
+      let self = this
+      if (!(this.username === "fake" && this.password === "fake123")) {
+        this.has_error = true
+        this.mensagem_login = "Usuário e senha inválido, tente novamente"
+      } else {
+        window.sessionStorage.setItem("isLogged", true)
+        this.mensagem_login = "Logado com sucesso ..."
+        setTimeout(function () {
+          self.$router.push({
+            path: self.$route.query.redirect
+          })
+        }, 1000)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .btn-show-password
+    padding: 0 3px;
+    margin-left: -50px;
+    height: 32px;
+    border-radius: 50px;
+  .row
+    margin 5px
+
+</style>
